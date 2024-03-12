cc7f26b502bbbcdb7499e3e623ba996688acba62
MohammedZuhairAhmed <zuhairahmed055@gmail.com>
2024-03-12 17:30:21 +0530

    FEAT : Filter added to Products page

--- src/components/CardsGrid.tsx
+++ src/components/CardsGrid.tsx
@@ -8,25 +8,70 @@ export default function CardsGrid({
   params,
   title,
   viewAllText,
+  override,
   redirectionLink,
 }: CardsGridProps) {
-  const [showAll, setShowAll] = useState(false);
+  const [showAll, setShowAll] = useState(override || false);
+  const [activeTag, setActiveTag] = useState('all');
 
   const handleClick = () => {
     setShowAll(!showAll);
   };
 
   const cardsToShow = showAll ? params : params.slice(0, 3);
-
   const showViewAll = params.length > 3;
+
+  const uniqueTags = new Set();
+  cardsToShow.forEach((prod) => uniqueTags.add(prod.tags[0]));
+
+  const handleAllClick = () => {
+    setActiveTag('all');
+  };
+
+  const handleTagClick = (tag: string) => {
+    setActiveTag(tag);
+  };
+
+  const filterButtons = Array.from(uniqueTags).map((tag) => {
+    return (
+      <button
+        key={tag as string}
+        className={`btn ${styles.filterButton} ${tag === activeTag ? 'btn-primary' : 'btn-outline-primary'}`}
+        onClick={() => handleTagClick(tag as string)}
+      >
+        {tag as string}
+      </button>
+    );
+  });
+
+  const filterProducts = (products: ProductProps[], activeTag: string) => {
+    if (activeTag === 'all') {
+      return products;
+    } else {
+      return products.filter((product) => product.tags[0] === activeTag);
+    }
+  };
 
   return (
     <div id="cards">
-      <div className={styles.Heading}>
-        <h2 className={styles.HeadingText}>{title}</h2>
-      </div>
+      {override ? (
+        <div className={styles.Heading}>
+          <h2 className={styles.HeadingText}>{title}</h2>
+          <button
+            className={`btn ${styles.filterButton} ${'all' === activeTag ? 'btn-primary' : 'btn-outline-primary'}`}
+            onClick={handleAllClick}
+          >
+            All
+          </button>
+          {filterButtons}
+        </div>
+      ) : (
+        <div className={styles.Heading}>
+          <h2 className={styles.HeadingText}>{title}</h2>
+        </div>
+      )}
       <div className={styles.cardsGrid}>
-        {cardsToShow.map((card, index) => (
+        {filterProducts(cardsToShow, activeTag).map((card: any, index: any) => (
           <Card key={index} {...card} redirectionLink={redirectionLink} />
         ))}
       </div>
@@ -34,7 +79,7 @@ export default function CardsGrid({
         onClick={handleClick}
         className={`body-10 mb-5 mt-2 ${styles.linkText}`}
       >
-        {showViewAll && (
+        {showViewAll && !override && (
           <span className={`link-default ${styles.purple}`}>
             {showAll ? 'Show Less' : viewAllText}
           </span>
