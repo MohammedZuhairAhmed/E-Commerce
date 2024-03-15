e0edec32fb323062bd5a3a76c96146c00d4437fb
MohammedZuhairAhmed <zuhairahmed055@gmail.com>
2024-03-15 16:24:30 +0530

    check 4

--- functions/user.js
+++
@@ -1,8 +0,0 @@
-import { getShortName } from './utils';
-
-export default function handler(request, response) {
-  const name = 'Hilary';
-  const shortName = getShortName(name);
-  const func = 'haha';
-  response.status(200).send({ name, shortName });
-}
