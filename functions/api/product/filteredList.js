ade8b344c76efedef8432daaa3292597590de3da
MohammedZuhairAhmed <zuhairahmed055@gmail.com>
2024-03-17 14:25:49 +0530

    check success

--- functions/api/product/filteredList.js
+++ functions/api/product/filteredList.js
@@ -1,12 +1,10 @@
 export default async function handler(req, res) {
-  // Directly use the data in the response body
-  const productTitles = req.body.map((product) => product.title);
+  const productData = req.body[0];
+  const productTitles = productData.map((product) => product.title);
 
-  // Prepare response (assuming JSON format)
   const response = {
     productTitles,
   };
 
-  // Send response efficiently
   res.status(200).json(response);
 }
