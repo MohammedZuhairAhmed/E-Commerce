a601cfcd1ace4c1bb4ef3c1df1f130de3deaa7c9
MohammedZuhairAhmed <zuhairahmed055@gmail.com>
2024-03-17 11:29:32 +0530

    env varaible check

---
+++ functions/api/product/filteredList.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const base_url = process.env.CONTENTSTACK_CDN;
+  const api_key = process.env.CONTENTSTACK_API_KEY;
+  const access_token = process.env.CONTENTSTACK_DELIVERY_TOKEN;
+  const environment = process.env.CONTENTSTACK_ENVIRONMENT;
+
+  const data = {
+    base_url,
+    api_key,
+    access_token,
+    environment,
+  };
+  res.status(200).json(data);
+}
