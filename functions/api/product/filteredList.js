fcc7644e9ee90f217f5845df54f6c8ad0adf7b69
MohammedZuhairAhmed <zuhairahmed055@gmail.com>
2024-03-17 11:38:08 +0530

    check2

--- functions/api/product/filteredList.js
+++ functions/api/product/filteredList.js
@@ -10,5 +10,7 @@ export default async function handler(req, res) {
     access_token,
     environment,
   };
-  res.status(200).json(data);
+
+  response.send(`Hello ${base_url}`);
+  //res.status(200).json(data);
 }
