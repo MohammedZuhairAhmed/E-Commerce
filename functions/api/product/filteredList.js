81384da1bc0600b7e7eb5d849f9779b5ce0d4353
MohammedZuhairAhmed <zuhairahmed055@gmail.com>
2024-03-17 11:38:43 +0530

    crcted

--- functions/api/product/filteredList.js
+++ functions/api/product/filteredList.js
@@ -11,6 +11,6 @@ export default async function handler(req, res) {
     environment,
   };
 
-  response.send(`Hello ${base_url}`);
+  res.send(`Hello ${base_url}`);
   //res.status(200).json(data);
 }
