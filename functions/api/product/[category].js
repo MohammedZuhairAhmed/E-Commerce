f844ee000927b3b19db763a206ea1f6e484b748e
MohammedZuhairAhmed <zuhairahmed055@gmail.com>
2024-03-19 16:15:15 +0530

    FEAT : More Optimised Approach by fetching categories and filter data through Contentstack SDK

--- functions/api/product/[category].js
+++ functions/api/product/[category].js
@@ -1,26 +1,26 @@
-export default async function handler(req, res) {
-  try {
-    if (req.body) {
-      const productData = req.body;
-      const categoryToFind = decodeURIComponent(req.query.category);
+// export default async function handler(req, res) {
+//   try {
+//     if (req.body) {
+//       const productData = req.body;
+//       const categoryToFind = decodeURIComponent(req.query.category);
 
-      const productIndexes = productData.reduce((acc, product, index) => {
-        if (product.tags[0] === categoryToFind) {
-          acc.push(index);
-        }
-        return acc;
-      }, []);
+//       const productIndexes = productData.reduce((acc, product, index) => {
+//         if (product.tags[0] === categoryToFind) {
+//           acc.push(index);
+//         }
+//         return acc;
+//       }, []);
 
-      const response = {
-        productIndexes: productIndexes,
-      };
+//       const response = {
+//         productIndexes: productIndexes,
+//       };
 
-      res.status(200).json(response);
-    } else {
-      res.status(400).json({ error: 'Empty request body' });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-}
+//       res.status(200).json(response);
+//     } else {
+//       res.status(400).json({ error: 'Empty request body' });
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// }
--- functions/api/product/categories.js
+++ functions/api/product/categories.js
@@ -1,17 +1,17 @@
-export default async function handler(req, res) {
-  if (req.body) {
-    const productData = req.body;
+// export default async function handler(req, res) {
+//   if (req.body) {
+//     const productData = req.body;
 
-    const productTags = [
-      ...new Set(productData.map((product) => product.tags[0])),
-    ];
+//     const productTags = [
+//       ...new Set(productData.map((product) => product.tags[0])),
+//     ];
 
-    const response = {
-      productTags,
-    };
+//     const response = {
+//       productTags,
+//     };
 
-    res.status(200).json(response);
-  } else {
-    res.status(400).json({ error: 'Empty request body' });
-  }
-}
+//     res.status(200).json(response);
+//   } else {
+//     res.status(400).json({ error: 'Empty request body' });
+//   }
+// }
